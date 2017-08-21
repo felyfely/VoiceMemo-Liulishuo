9ce1468b032d18708629bc368b9252d32cddfe53
fudan <felydefely@gmail.com>
2017-08-21 19:03:57 +0800

    Initial Commit

---
+++ VoiceMemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VoiceMemo
+//
+//  Created by 付 旦 on 8/21/17.
+//  Copyright © 2017 付 旦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
